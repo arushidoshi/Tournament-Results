8963bffc2896c9b27546a2f82e56b3d2d1f514b8
Arushi Doshi <arushidoshi@gmail.com>
2016-08-21 21:14:14 +0530

    Remove redundancy and normalize database

--- tournament.sql
+++ tournament.sql
@@ -9,10 +9,6 @@ CREATE DATABASE tournament;
 -- Connect to the tournament database
 \connect tournament
 
--- DROP old Tables
-DROP TABLE IF EXISTS Players CASCADE;
-DROP TABLE IF EXISTS Matches CASCADE;
-
 -- Players Table
 CREATE TABLE Players (
     id SERIAL primary key,
@@ -23,28 +19,13 @@ CREATE TABLE Players (
 CREATE TABLE Matches (
     id SERIAL primary key,
     winner int references Players(id),
-    loser int references Players(id),
-    result int
+    loser int references Players(id)
 );
-
--- Wins View shows number of wins for each Player
-CREATE VIEW Wins AS
-    SELECT Players.id, COUNT(Matches.winner) AS won
-    FROM Players
-    LEFT JOIN (SELECT * FROM Matches WHERE result>0) as Matches
-    ON Players.id = Matches.winner
-    GROUP BY Players.id;
-
--- Count View shows number of matches for each Player
-CREATE VIEW Count AS
-    SELECT Players.id, Count(Matches.loser) AS match_played
-    FROM Players
-    LEFT JOIN Matches
-    ON Players.id = Matches.winner
-    GROUP BY Players.id;
 
 -- Standings View shows number of wins and matches for each Player
 CREATE VIEW Standings AS
-    SELECT Players.id,Players.name,Wins.won,Count.match_played
-    FROM Players,Count,Wins
-    WHERE Players.id = Wins.id and Wins.id = Count.id;
+SELECT Players.id, Players.name,
+(SELECT count(*) FROM Matches WHERE Matches.winner = Players.id) as won,
+(SELECT count(*) FROM Matches WHERE Players.id in (winner, loser)) as total_matches
+FROM Players
+GROUP BY Players.id;
